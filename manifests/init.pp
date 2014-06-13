9fb0915fe68eec67abf3e241d13ce3aae407d65c
wbs75 <wbs75@me.com>
2014-06-12 19:20:41 -0600

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@ describe 'totalfinder' do
   it do
     should contain_package('TotalFinder').with({
       :provider => 'pkgdmg',
-      :source   => 'http://downloads.binaryage.com/TotalFinder-1.6.dmg'
+      :source   => 'http://downloads-2.binaryage.com/TotalFinder-1.6.dmg'
     })
   end
 end
