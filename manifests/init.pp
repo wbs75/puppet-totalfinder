67debcf6c6c063dc8fc3295217d5ca9ab62d1409
wbs75 <wbs75@me.com>
2014-06-12 19:03:45 -0600

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,12 @@
-class totalfinder(
-  $version = '1.1.18',
-) {
-  package { "TotalFinder_${version}":
-    ensure   => installed,
-    source   => "http://downloads.binaryage.com/TotalFinder-${version}.dmg",
-    provider => 'pkgdmg'
-  }
-}
+require 'spec_helper'
+
+describe 'totalfinder' do
+    let (:params) {{:completion => true}}
+
+  it do
+    should contain_package('TotalFinder').with({
+      :provider => 'pkgdmg',
+      :source   => 'http://downloads.binaryage.com/TotalFinder-1.6.dmg'
+    })
+  end
+end
